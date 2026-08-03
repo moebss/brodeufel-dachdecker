7beceacbe40ec5748db91cd86ab4a67970153366
Alexander Rheindorf <Alexander.Rheindorf@honestis.ag>
2026-08-03 22:47:16 +0200

    Initial website commit

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/brodeufel-dachdecker/',
+  server: {
+    port: 3007,
+    host: true
+  },
+  preview: {
+    port: 3007,
+    host: true
+  }
+});
